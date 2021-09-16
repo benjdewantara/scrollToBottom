5bac5321aac32a8853365b97e293a202b0e76393
Benyamin Manullang <benjdewantara@gmail.com>
2021-09-16 23:44:36 +0700

    Auto-format

--- content_scripts/scrollToBottom.js
+++ content_scripts/scrollToBottom.js
@@ -1,97 +1,96 @@
-(function() {
-    /* Check if the script is injected
-    */
+(function () {
+  /* Check if the script is injected
+  */
 
-    if (window.hasScript) {
-        return;
+  if (window.hasScript) {
+    return;
+  }
+  window.hasScript = true;
+
+  var currentlyScrollingInterval;
+  var tabId;
+
+  // console.log("scrollToBottom.js: Js is injected..");
+
+
+  function scrollToBottom() {
+    var maxScrollHeight = window.scrollMaxY;
+    window.scrollTo(0, maxScrollHeight);
+  }
+
+  function toggleScrolling() {
+    if (window.isScrollingToBottomFox) {
+      window.clearInterval(currentlyScrollingInterval);
+      browser.runtime.sendMessage({
+        msg: "notRunning"
+      });
+      window.isScrollingToBottomFox = false;
+
+      console.log("scrollToBottom.js: Sending message delTabFromScrollingList with tabId = " + tabId);
+
+      browser.runtime.sendMessage({
+        msg: "delTabFromScrollingList",
+        tabId: tabId
+      });
+      // console.log("scrollToBottom.js: scrollToBottom is not running");
+    } else {
+      currentlyScrollingInterval = window.setInterval(scrollToBottom, 1000);
+      browser.runtime.sendMessage({
+        msg: "yesRunning"
+      });
+      window.isScrollingToBottomFox = true;
+
+      // console.log("scrollToBottom.js: Sending message addTabToScrollingList with tabId = " + tabId);
+
+      browser.runtime.sendMessage({
+        msg: "addTabToScrollingList",
+        tabId: tabId
+      });
+      // console.log("scrollToBottom.js: scrollToBottom is running");
     }
-    window.hasScript = true;
-    
-    var currentlyScrollingInterval;
-    var tabId;
+  }
 
-    // console.log("scrollToBottom.js: Js is injected..");
-    
-    
-    function scrollToBottom() {
-        var maxScrollHeight = window.scrollMaxY;
-        window.scrollTo(0, maxScrollHeight);
+  /* Listening to messages from background script */
+  browser.runtime.onMessage.addListener((message) => {
+    if (message.msg == "isRunning") {
+      if (window.isScrollingToBottomFox) {
+        browser.runtime.sendMessage({
+          msg: "yesRunning"
+        });
+      } else {
+        browser.runtime.sendMessage({
+          msg: "notRunning"
+        });
+      }
+    } else if (message.msg == "toggle-scrolling") {
+      tabId = message.tabId;
+      toggleScrolling();
     }
+  });
 
-    function toggleScrolling() {
-        if (window.isScrollingToBottomFox) {
-            window.clearInterval(currentlyScrollingInterval);
-            browser.runtime.sendMessage({
-                msg: "notRunning"
-            });
-            window.isScrollingToBottomFox = false;
-            
-            // console.log("scrollToBottom.js: Sending message delTabFromScrollingList with tabId = " + tabId);
-            
-            browser.runtime.sendMessage({
-                msg: "delTabFromScrollingList",
-                tabId: tabId
-            });
-            // console.log("scrollToBottom.js: scrollToBottom is not running");
+  var handleKeydown = function (e) {
+    // Dealing with double-click of "End" key
+    if (e.key == "End") {
+      var timeStamp = e.timeStamp;
+
+      if (this.timeStampBefore == null) {
+        this.timeStampBefore = timeStamp;
+      } else {
+        var delta = timeStamp - this.timeStampBefore;
+
+        if (delta <= 200) {
+          // Perform auto-scroll here
+          toggleScrolling();
         }
-        else {
-            currentlyScrollingInterval = window.setInterval(scrollToBottom, 1000);
-            browser.runtime.sendMessage({
-                msg: "yesRunning"
-            });
-            window.isScrollingToBottomFox = true;
-            
-            // console.log("scrollToBottom.js: Sending message addTabToScrollingList with tabId = " + tabId);
-            
-            browser.runtime.sendMessage({
-                msg: "addTabToScrollingList",
-                tabId: tabId
-            });
-            // console.log("scrollToBottom.js: scrollToBottom is running");
-        }
+
+        this.timeStampBefore = null;
+      }
+    } else if (e.key == "Home") {
+      if (window.isScrollingToBottomFox) {
+        toggleScrolling();
+      }
     }
+  };
 
-    /* Listening to messages from background script */
-    browser.runtime.onMessage.addListener((message) => {
-        if (message.msg == "isRunning") {
-            if (window.isScrollingToBottomFox) {
-                browser.runtime.sendMessage({
-                    msg: "yesRunning"
-                });
-            } else {
-                browser.runtime.sendMessage({
-                    msg: "notRunning"
-                });
-            }
-        } else if (message.msg == "toggle-scrolling") {
-            tabId = message.tabId;
-            toggleScrolling();
-        }
-    });
-
-    var handleKeydown = function(e) {
-        // Dealing with double-click of "End" key
-        if (e.key == "End") {
-            var timeStamp = e.timeStamp;
-
-            if (this.timeStampBefore == null) {
-                this.timeStampBefore = timeStamp;
-            } else {
-                var delta = timeStamp - this.timeStampBefore;
-
-                if (delta <= 200) {
-                    // Perform auto-scroll here
-                    toggleScrolling();
-                }
-                
-                this.timeStampBefore = null;
-            }
-        } else if (e.key == "Home") {
-            if (window.isScrollingToBottomFox) {
-                toggleScrolling();
-            }
-        }
-    };
-
-    // document.body.addEventListener("keydown", handleKeydown, false);
+  // document.body.addEventListener("keydown", handleKeydown, false);
 })();
